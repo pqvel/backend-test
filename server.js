20dc02d58a76f1282ea099db27683ef2e185e9c4
Pavel Salianik <solyqnik@gmail.com>
2025-01-08 12:10:33 +0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { data } from "./data.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Define a simple route
